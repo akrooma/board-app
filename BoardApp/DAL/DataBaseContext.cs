2573b4d87d3a31d01e1cc1fac013195267f37368
Ants Kristjan Rooma <antskristjan@hotmail.com>
2016-03-03 22:22:34 +0200

    More changes things stuff.

---
+++ BoardApp/DAL/DataBaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Migrations;
+using Domain;
+
+namespace DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("name=BoardAppDbConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataBaseContext, DbMigrationConfiguration>());
+        }
+
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<DeckStyle> DeckStyles { get; set; }
+        public DbSet<RidingStyle> RidingStyles { get; set; }
+        public DbSet<MapPoint> MapPoints { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<RouteComment> RouteComments { get; set; }
+        public DbSet<RouteCharacteristic> RouteCharacteristics { get; set; }
+        public DbSet<RouteAndCharacteristic> RouteAndCharacteristics { get; set; }
+        public DbSet<RouteInEvent> RouteInEvents { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
