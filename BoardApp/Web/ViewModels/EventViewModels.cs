8dd8c13cc68c1906ae05076cf69f2b46f86fba47
Ants Kristjan Rooma <antskristjan@hotmail.com>
2016-06-14 01:29:43 +0300

    DateTimeEditor's don't work in the intended manner right now.. Adding events basically works like adding new routes.

---
+++ BoardApp/Web/ViewModels/EventViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain;
+
+namespace Web.ViewModels
+{
+    public class EventCreateViewModel
+    {
+        public Event Event { get; set; }
+        public List<int> PlannedRouteIds { get; set; }
+        public MultiSelectList RouteSelectList { get; set; }
+    }
+}
